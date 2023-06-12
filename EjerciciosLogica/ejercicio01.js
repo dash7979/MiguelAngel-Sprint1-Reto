06cc207b5df852dad0a4b34db3ca3ef996008f81
“AngelOcampo” <miguequinteroocampo@gmail.com>
2023-06-12 16:02:43 -0500

    Cambio ejercicio, pongo dos ejemplos

--- EjerciciosLogica/ejercicio01.js
+++ EjerciciosLogica/ejercicio01.js
@@ -4,3 +4,10 @@ function next_Pal(a,b) {
 
 let multiplicar = next_Pal(11, 2);
 console.log(multiplicar);
+
+function next_Pal2(c,d) {
+    return c + d
+}
+
+const sumar = next_Pal2(188, 3)
+console.log(sumar);
