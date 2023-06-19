33dfd069be96dd539cb3525490b59fff583cdba8
“AngelOcampo” <miguequinteroocampo@gmail.com>
2023-06-19 11:39:22 -0500

    ejercicio 4 realizado y cambios en el primer ejercicio

--- EjerciciosLogica/ejercicio01.js
+++ EjerciciosLogica/ejercicio01.js
@@ -1,15 +1,27 @@
-function next_Pal(a,b) {
-    return a * b
-}
+// Un número palíndromo es aquel que se lee de la misma manera de izquierda a
+// derecha y de derecha a izquierda.
+// En otras palabras, es un número que se mantiene igual cuando sus dígitos son
+// invertidos.
+// Ejemplos: 121, 12321 y 333.
+// En este desafío, se te pide que crees una función llamada "next_pal()" que
+// reciba como parámetro un número entero positivo, tu objetivo es que la
+// función retorné el siguiente número palíndromo a partir del recibido por
+// parámetro.
 
-let multiplicar = next_Pal(11, 2);
-console.log(multiplicar);
 
-function next_Pal2(c,d) {
-    return c + d
-}
 
-const sumar = next_Pal2(188, 3)
-console.log(sumar);
+// function next_Pal(a,b) {
+//     return a * b
+// }
 
-//cambio de rama
+// let multiplicar = next_Pal(11, 2);
+// console.log(multiplicar);
+
+// function next_Pal2(c,d) {
+//     return c + d
+// }
+
+// const sumar = next_Pal2(188, 3)
+// console.log(sumar);
+
+// //cambio de rama
